5fca83a755db6bc4131b2c8e941a6cd37f0c1cb8
Hemanth <hemanthmanikantabunga@gmail.com>
2019-07-19 20:07:21 +0530

    python: Fixed Calculator Derp
    - Clean up unused comments
    - Fixed the output

--- calculator.py
+++ calculator.py
@@ -1,34 +1,19 @@
 # Python program to make a simple Calculator
-def calcu(choice):
+def calcu(choice,inp1,inp2):
         switcher={
-                1: print(f'Addition of {inp1} and {inp2}:{inp1+inp2}'),
-                2: print(f'Subtraction of {inp1} and {inp2}:{inp1-inp2}'),
-                3: print(f'Multiplication of {inp1} and {inp2}:{inp1*inp2}'),
-                4: print(f'Division of {inp1} and {inp2}:{inp1/inp2}'),
-             }
-        #print(switcher.get(choice,'Please read the title and enter values correctly'))
+                '1': f'Addition of {inp1} and {inp2}:{inp1+inp2}',
+                '2': f'Subtraction of {inp1} and {inp2}:{inp1-inp2}',
+                '3': f'Multiplication of {inp1} and {inp2}:{inp1*inp2}',
+                '4': f'Division of {inp1} and {inp2}:{inp1/inp2}'
+        }
+        if(choice in switcher):
+                print(inp1,choice,inp2,"=",switcher[choice])
+        else:
+                print("Please read the title and enter values correctly")       
 
 print('Please Enter two values')
 inp1 = int(input('Value of A:'))
 inp2 = int(input('Value of B:'))
 print('Simple Calculator \n1.Addition \n2.Subtraction \n3.Multiplication \n4.Division')
 choice = input('Select one option:')
-calcu(choice)
-
-
-# switch (choice):
-# case 1 : 
-    
-#     break
-# case 2 :
-#     
-#     break
-# case 3 :
-#     
-#     break
-# case 4 :
-#     
-#     break
-# default :
-#     
-#     break
+calcu(choice,inp1,inp2) 
